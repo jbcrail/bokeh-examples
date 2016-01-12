9a6c00559a95dd16a52f6332ab62bc7cb9993555
Joseph Crail <jbcrail@gmail.com>
2016-01-11 22:36:29 -0500

    Add subplot for precipitation

--- app/weather/main.py
+++ app/weather/main.py
@@ -1,10 +1,12 @@
+from __future__ import division
 from os.path import join, dirname
+
 import numpy as np
 import pandas as pd
 
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select
-from bokeh.palettes import Blues4
+from bokeh.models import ColumnDataSource, DataRange1d, Range1d, VBox, HBox, Select, GridPlot
+from bokeh.palettes import Blues4, Greens4
 from bokeh.plotting import Figure
 from scipy.signal import savgol_filter
 
@@ -17,6 +19,7 @@ def get_dataset(src, name, distribution):
     df['date'] = pd.to_datetime(df.date)
     df['left'] = df.date - pd.DateOffset(days=0.5)
     df['right'] = df.date + pd.DateOffset(days=0.5)
+    df['actual_precipitation'] = df.actual_precipitation / 100
     df = df.set_index(['date'])
     df.sort_index(inplace=True)
     if distribution == 'Smooth':
@@ -27,8 +30,8 @@ def get_dataset(src, name, distribution):
     return ColumnDataSource(data=df)
 
 
-def make_plot(source, title):
-    plot = Figure(x_axis_type="datetime", plot_width=1000, tools="", toolbar_location=None)
+def make_temperature_plot(source, title):
+    plot = Figure(x_axis_type="datetime", plot_width=1000, min_border_bottom=0, toolbar_location=None)
     plot.title = title
     colors = Blues4[0:3]
 
@@ -38,7 +41,7 @@ def make_plot(source, title):
 
     # fixed attributes
     plot.border_fill_color = "whitesmoke"
-    plot.xaxis.axis_label = None
+    plot.xaxis.visible = None
     plot.yaxis.axis_label = "Temperature (F)"
     plot.axis.major_label_text_font_size = "8pt"
     plot.axis.axis_label_text_font_size = "8pt"
@@ -50,10 +53,30 @@ def make_plot(source, title):
     return plot
 
 
+def make_precipitation_plot(source):
+    plot = Figure(x_axis_type="datetime", plot_width=1000, plot_height=125, min_border_left=50, min_border_right=50, min_border_top=0, min_border_bottom=0, toolbar_location=None)
+    plot.title = None
+
+    plot.quad(top='actual_precipitation', bottom=0, left='left', right='right', color=Greens4[1], source=source)
+
+    # fixed attributes
+    plot.border_fill_color = "whitesmoke"
+    plot.yaxis.axis_label = "Precipitation (in)"
+    plot.axis.major_label_text_font_size = "8pt"
+    plot.axis.axis_label_text_font_size = "8pt"
+    plot.axis.axis_label_text_font_style = "bold"
+    plot.x_range = DataRange1d(range_padding=0.0, bounds=None)
+    plot.y_range = DataRange1d(range_padding=0.0, bounds=None)
+    plot.grid.grid_line_alpha = 0.3
+    plot.grid[0].ticker.desired_num_ticks = 12
+
+    return plot
+
+
 # set up callbacks
 def update_plot(attrname, old, new):
     city = city_select.value
-    plot.title = cities[city]['title']
+    tplot.title = cities[city]['title']
 
     src = get_dataset(df, cities[city]['airport'], distribution_select.value)
     source.data.update(src.data)
@@ -82,7 +105,8 @@ distribution_select = Select(value=distribution, title='Distribution', options=[
 
 df = pd.read_csv(join(dirname(__file__), 'data/2015_weather.csv'))
 source = get_dataset(df, cities[city]['airport'], distribution)
-plot = make_plot(source, cities[city]['title'])
+tplot = make_temperature_plot(source, cities[city]['title'])
+pplot = make_precipitation_plot(source)
 
 city_select.on_change('value', update_plot)
 distribution_select.on_change('value', update_plot)
@@ -90,4 +114,4 @@ distribution_select.on_change('value', update_plot)
 controls = VBox(city_select, distribution_select)
 
 # add to document
-curdoc().add_root(HBox(controls, plot))
+curdoc().add_root(HBox(controls, GridPlot(children=[[tplot], [pplot]], toolbar_location=None), width=1200))
